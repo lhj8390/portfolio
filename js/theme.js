1cfcc491b1daab89555cd6663a5e9d77ca983f80
DESKTOP-DLKD9H9\user <lhj8390@naver.com>
2020-10-20 23:45:32 +0900

    add view details

--- js/theme.js
+++ js/theme.js
@@ -67,15 +67,7 @@
         }
     }
     clients_slider();
-	/*----------------------------------------------------*/
-    /*  MailChimp Slider
-    /*----------------------------------------------------*/
-    function mailChimp(){
-        $('#mc_embed_signup').find('form').ajaxChimp();
-    }
-    mailChimp();
 	
-	$('select').niceSelect();
 	
 	/*----------------------------------------------------*/
     /*  Simple LightBox js
